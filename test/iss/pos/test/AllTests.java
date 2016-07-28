0cc522f9d5ff45292333bbf23fba805116319383
Hans <yunghans@hotmail.com>
2016-07-28 13:24:49 +0800

    Create AllTests test suite

---
+++ test/iss/pos/test/AllTests.java
@@ -0,0 +1,13 @@
+package iss.pos.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({DiscountTest.class, SampleTest.class})
+public class AllTests {
+
+}
